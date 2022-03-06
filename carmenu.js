4b62167139e7a6154ae70b4edd9c934696cef42f
Your Name <you@example.com>
2022-03-06 08:41:21 -0600

    notes

--- carmenu.js
+++ carmenu.js
@@ -1,22 +1,20 @@
 class Car {
-    constructor(make, model, msrp, mpg){
+    constructor(make, model, msrp, mpg) {
         this.make = make;
         this.model = model;
         this.msrp = msrp;
         this.mpg = mpg;
     }
-    describe(){
+    describe() {
         return `The ${this.make} ${this.model} costs around ${this.msrp} and gets ${this.mpg} mpg`
     }
 }
 
 class Menu {
-    constructor(){
-        this.cars =[]; //stores all Car objects that are created
+    constructor() {
+        this.cars = []; //stores all Car objects that are created
     }
-}
-
-    mainMenu(){
+    mainMenu() {
         let choice = prompt(`
         0) Exit Program
         1) Add a car
@@ -24,27 +22,27 @@ class Menu {
         3) update a car
         4) view all cars
         `)
+        switch (choice) {
+            case "1":
+                this.addCar(); //have to have a this keyword
+                break;
+            case "2":
+                this.deleteCar();
+                break;
+            case "3":
+                this.updatedCar();
+                break;
+            case "4":
+                this.viewCars();
+                break;
+            case "0":
+                this.exitProgram();
+                break;
+            default:
+                this.mainMenu();
+        }
     }
-    switch (choice) {
-        case "1":
-            this.addCar(); //have to have a this keyword
-            break;
-        case "2":
-            this.deleteCar();
-            break;
-        case "3":
-            this.updatedCar();
-            break;
-        case "4":
-            this.viewCars();
-            break;
-        case "0":
-            this.exitProgram();
-            break;
-        default:
-            this.mainMenu();
-    }
-
+    
     addCar() {
         const newCar = new Car (
             prompt("Enter Make: "),
